8b33c16d7daf224caad01f15c4e20b70e5cf59f6
Marco-ing <54912771+Marco-ing@users.noreply.github.com>
2022-07-08 15:56:46 -0500

    Inicializacion del servidor

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('Hello word con express')
+})
+
+
+
+
+app.listen(3000);
+console.log('Server on port 3000');
